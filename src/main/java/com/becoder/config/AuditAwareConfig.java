9b1e038c138b6bdc33fb1b50210f554db73b918c
Arush6 <147136648+Arush6@users.noreply.github.com>
2025-07-19 06:50:46 +0530

    auditing and exist implementation

---
+++ src/main/java/com/becoder/config/AuditAwareConfig.java
@@ -0,0 +1,18 @@
+package com.becoder.config;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuditAwareConfig implements AuditorAware<Integer> {
+
+	@Override
+	public Optional<Integer> getCurrentAuditor() {
+		// TODO Auto-generated method stub
+		return Optional.of(2);
+	}
+
+	
+}
--- src/main/java/com/becoder/entity/BaseModel.java
+++ src/main/java/com/becoder/entity/BaseModel.java
@@ -2,6 +2,14 @@ package com.becoder.entity;
 
 import java.util.Date;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,13 +17,21 @@ import lombok.Setter;
 @Getter
 @Setter
 @MappedSuperclass
-public class BaseModel {
 
-	private Boolean isActive;
-	private Boolean isDeleted;
+public abstract class BaseModel {
+
+
+	@CreatedBy
+	@Column(updatable = false)
 	private Integer createdBy;
+	@LastModifiedBy
+	@Column(insertable = false)
 	private Integer updatedBy;
+	@CreatedDate
+	@Column(updatable = false)
 	private Date createdOn;
+	@LastModifiedDate
+	@Column(insertable = false)
 	private Date updatedOn;
 
 }
