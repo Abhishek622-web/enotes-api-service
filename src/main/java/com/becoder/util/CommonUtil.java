410473816a29d14027447e3096693ea19d142552
Arush6 <147136648+Arush6@users.noreply.github.com>
2025-07-19 07:40:35 +0530

    Generic Response implementation

---
+++ src/main/java/com/becoder/util/CommonUtil.java
@@ -0,0 +1,47 @@
+package com.becoder.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.becoder.handler.GenericResponse;
+
+public class CommonUtil   {
+
+	
+	public static ResponseEntity<?> createBuildResponse(Object data,HttpStatus status){
+		
+		GenericResponse response = new GenericResponse().builder()
+				.responseStatus(status)
+				.status("success")
+				.message("success")
+				.data(data)
+				.build();	
+		return response.create();			
+	}
+	public static ResponseEntity<?> createBuildResponseMessage(String message,HttpStatus status){		
+		GenericResponse response = new GenericResponse().builder()
+				.responseStatus(status)
+				.status("success")
+				.message(message)
+				.build();		
+		return response.create();			
+	}
+	public static ResponseEntity<?> createErrorResponse(HttpStatus status,Object data ){		
+		GenericResponse response = new GenericResponse().builder()
+				.responseStatus(status)
+				.status("failed")
+				.message("failed")
+				.build();		
+		return response.create();			
+	}
+	
+	public static ResponseEntity<?> createErrorResponseMessage(String message,HttpStatus status){		
+		GenericResponse response = new GenericResponse().builder()
+				.responseStatus(status)
+				.status("failed")
+				.message(message)
+				.build();		
+		return response.create();			
+	}
+	
+}
